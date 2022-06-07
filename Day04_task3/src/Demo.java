8f61e0f49be5aa0fce9a4cf318c70be55d1d424c
ramshad <ramshad@DESKTOP-UDKROH6>
2022-06-07 23:48:30 +0530

    first message

---
+++ Day04_task3/src/Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+	String x;
+	String y;
+	public static void main(String[] args) {
+		Demo myobj1 = new Demo();
+		Demo myobj2 = new Demo();
+		myobj1.x = "hello";
+		myobj2.y = "Ramshu";
+		System.out.println(myobj1.x +" "+ myobj2.y);
+	}
+
+}
