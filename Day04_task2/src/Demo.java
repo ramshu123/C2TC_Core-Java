3c3d19cd642a517cfff3eea9e6001c15e90c2ac8
ramshad <ramshad@DESKTOP-UDKROH6>
2022-06-07 23:47:50 +0530

    first message

---
+++ Day04_task2/src/Demo.java
@@ -0,0 +1,22 @@
+
+public class Demo {
+	int x;
+	String y;
+	public static void main(String[] args) {
+		Demo myobj1 = new Demo();
+		Demo myobj2 = new Demo();
+		Demo myobj3 = new Demo();
+		Demo myobj4 = new Demo();
+		myobj1.x = 24;
+		myobj2.x = 55;
+		myobj3.x = 33;
+		myobj4.y = "hello";
+		System.out.println(myobj1.x);
+		System.out.println(myobj2.x);
+		System.out.println(myobj3.x);
+		System.out.println(myobj4.y);
+		
+
+	}
+
+}
