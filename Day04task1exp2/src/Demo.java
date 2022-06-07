53bee6eb3111fc6d9e9a0aa0aa94e3fca3ff7cd1
ramshad <ramshad@DESKTOP-UDKROH6>
2022-06-07 23:47:00 +0530

    first message

---
+++ Day04task1exp2/src/Demo.java
@@ -0,0 +1,15 @@
+
+public class Demo {
+	int x;
+	public static void main(String[] args) {
+		Demo myobj1 = new Demo();
+		Demo myobj2 = new Demo();
+		myobj1.x = 24;
+		myobj2.x = 55;
+		System.out.println(myobj1.x);
+		System.out.println(myobj2.x);
+		
+
+	}
+
+}
