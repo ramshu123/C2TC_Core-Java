90267b4c95502361372a61673c852f325693c803
ramshad <ramshad@DESKTOP-UDKROH6>
2022-06-08 00:12:29 +0530

    first message

---
+++ Day05_task01/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Apple a1 = new Apple();
+		Apple a2 = new Apple();
+		a1.weight = 1;
+		a2.weight =2;
+		System.out.println(a2.weight);
+		
+
+	}
+
+}
